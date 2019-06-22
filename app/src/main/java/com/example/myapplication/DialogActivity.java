4472daa95fed7b73a8c72affba0fcd735107933c
tariksuid <tariksuid@gmail.com>
2019-06-22 23:35:39 +0300

    initial version

---
+++ app/src/main/java/com/example/myapplication/DialogActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DialogActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_dialog);
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+ import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int CAMERA_REQUEST = 1888 , PICK_IMAGE=2;
+    private ImageView   imageView, imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9 ;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+   // int height , width ;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView1 = (ImageView)this.findViewById(R.id.imageView1);
+        imageView2 = (ImageView)this.findViewById(R.id.imageView2);
+        imageView3 = (ImageView)this.findViewById(R.id.imageView3);
+        imageView4 = (ImageView)this.findViewById(R.id.imageView4);
+        imageView5 = (ImageView)this.findViewById(R.id.imageView5);
+        imageView6 = (ImageView)this.findViewById(R.id.imageView6);
+        imageView7 = (ImageView)this.findViewById(R.id.imageView7);
+        imageView8 = (ImageView)this.findViewById(R.id.imageView8);
+        imageView9 = (ImageView)this.findViewById(R.id.imageView9);
+
+  //ImageView img  = findViewById(R.id.imageView1)  ;
+/*height =   img.getHeight() ;
+  width = img.getWidth();*/
+
+    }
+
+    public void onClickDialog(View v)
+    {
+/*
+Intent intent = new Intent (this , DialogActivity.class) ;
+
+startActivity(intent);
+*/ if (v.getId() == R.id.imageView1)
+        imageView=imageView1;
+    else if (v.getId() == R.id.imageView2)
+        imageView=imageView2;
+    else if (v.getId() == R.id.imageView3)
+        imageView=imageView3;
+    else if (v.getId() == R.id.imageView4)
+        imageView=imageView4;
+    else if (v.getId() == R.id.imageView5)
+        imageView=imageView5;
+    else if (v.getId() == R.id.imageView6)
+        imageView=imageView6;
+    else if (v.getId() == R.id.imageView7)
+        imageView=imageView7;
+    else if (v.getId() == R.id.imageView8)
+        imageView=imageView8;
+    else
+        imageView=imageView9;
+
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.activity_dialog);
+        dialog.setTitle("GET IMAGE");
+
+       // EditText textViewUser = new EditText(getApplicationContext());
+       // textViewUser = (TextView) findViewById(R.id.textBrand);
+       // textViewUser.setText("Hi");
+
+        dialog.show();
+
+    }
+
+
+    public void onClick2 (View v )
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+
+    }
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        public void onClick( View v)
+        {
+
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+            }
+            else
+            {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode != 1) {
+
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+        {
+            Log.d("re" , "requestCode!=ok");
+        }
+
+    }
+
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            /*int targetW = imageView.getWidth();
+            int targetH = imageView.getHeight();
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+             int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            // Determine how much to scale down the image
+            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            bmOptions.inPurgeable = true;*/
+
+
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            Bitmap bMapScaled = Bitmap.createScaledBitmap(photo, 300, 300, true);
+
+                 imageView.setImageBitmap(bMapScaled);
+
+
+        }
+
+        if (requestCode == 2) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null) {
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
+
+                        Bitmap bMapScaled = Bitmap.createScaledBitmap(bitmap, 300, 300, true);
+
+                        imageView.setImageBitmap(bMapScaled);
+                       // imageView.setImageBitmap(bitmap);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (resultCode == Activity.RESULT_CANCELED)  {
+                Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+
+}
